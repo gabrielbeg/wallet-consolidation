c876ac2f36182ce7c01deb1e6de3d7179da0fbba
Gabriel Begueldo <gabrielbeg@live.com>
2025-03-30 21:37:33 -0300

    Update next config so it can be built on amplify.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        // Handle PDF renderer
+        config.resolve.alias.canvas = false;
+        config.resolve.alias.encoding = false;
 
-export default nextConfig;
+        return config;
+    },
+    transpilePackages: ["@react-pdf/renderer"],
+};
+  
+  
+  export default nextConfig;
+  
